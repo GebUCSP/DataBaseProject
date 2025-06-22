fbf313149ea2bf3382af7b9fba812e317aa4dfcd
Geb <jose.cornejo.castro@ucsp.edu.com>
2025-06-22 12:02:53 -0500

    storage modification

--- DataBaseProject/Storage.h
+++ DataBaseProject/Storage.h
@@ -1,10 +1,15 @@
 #pragma once
 #include <vector>
+
 using namespace std;
 
 template <class T>
 class Storage
 {
-	vector<T> container;
+public:
+	Storage(Storage<T>* father_ = nullptr, int container_size);
+private:
+	Storage<T>* father;
+	vector<T*> container;
 };
 
